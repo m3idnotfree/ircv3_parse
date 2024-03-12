1ae2d08bf22fb392208b948b212f2bac24a60457
m3id <johndoe@example.com>
2024-03-12 13:43:04 +0900

    feat: more flexible lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@
 //! # Example
 //! ```no_run
 //! use std::collections::HashMap;
-//! use ircv3_parse::{Ircv3Parse, Ircv3Params, ChannelnMsg};
+//! use ircv3_parse::ircv3_parse;
 //! let msg = "@badge-info=;badges=broadcaster/1;client-nonce=997dcf443c31e258c1d32a8da47b6936;color=#0000FF;display-name=abc;emotes=;first-msg=0;flags=0-6:S.7;id=eb24e920-8065-492a-8aea-266a00fc5126;mod=0;room-id=713936733;subscriber=0;tmi-sent-ts=1642786203573;turbo=0;user-id=713936733;user-type= :abc!abc@abc.tmi.twitch.tv PRIVMSG #xyz :HeyGuys\r\n";
-//! let result = Ircv3Parse::new(msg);
+//! let (tags, prefix, command, params) = ircv3_parse(msg);
 //! let expeced_tags= HashMap::from([
 //!     ("badge-info", ""),
 //!     ("subscriber", "0"),
@@ -25,184 +25,38 @@
 //!     ("user-type", "")]);
 //!
 //!
-//! let binding = ChannelnMsg::new("#xyz", "HeyGuys");
 //!
-//! assert_eq!(result.prefix.to_str(), Some(("abc", Some("abc@abc.tmi.twitch.tv"))));
-//! assert_eq!(result.command, "PRIVMSG");
-//! assert_eq!(result.params.channel_n_message(), Ok(("\r\n", binding)));
+//! assert_eq!(prefix.as_ref(), &Some(("abc", Some("abc@abc.tmi.twitch.tv"))));
+//! assert_eq!(command, "PRIVMSG");
+//! assert_eq!(params.channel(), "#xyz");
+//! assert_eq!(params.message(), "HeyGuys");
 //!
 //!```
-// use std::collections::HashMap;
+use ircv3_tags::{tags_parse, IRCv3Tags};
 
-use nom::{
-    branch::alt,
-    bytes::complete::{tag, take_until, take_while},
-    character::complete::{crlf, not_line_ending, space1},
-    combinator::{eof, opt},
-    multi::separated_list1,
-    sequence::{delimited, preceded, terminated, tuple},
-    IResult,
-};
+use nom::{bytes::complete::take_until, sequence::tuple, IResult};
 
-pub use ircv3_tags::Ircv3TagsParse;
+mod prefix;
+pub use prefix::*;
+mod params;
+pub use params::*;
 
-#[derive(Debug, PartialEq)]
-pub struct Ircv3Parse<'a> {
-    pub tags: Ircv3TagsParse<'a>,
-    pub prefix: Ircv3Prefix<'a>,
-    pub command: &'a str,
-    pub params: Ircv3Params<'a>,
-}
-
-impl<'a> Ircv3Parse<'a> {
-    pub fn new(msg: &str) -> Ircv3Parse {
-        let (_, result) = Ircv3Parse::parse(msg).unwrap();
-
-        result
-    }
-
-    pub fn parse_lists(msg: &str) -> IResult<&str, Vec<Ircv3Parse>> {
-        separated_list1(crlf, Ircv3Parse::parse)(msg)
-    }
-
-    pub fn parse(msg: &str) -> IResult<&str, Ircv3Parse> {
-        let (remain, (tags, prefix, command, params)) = tuple((
-            Ircv3TagsParse::parse,
-            Ircv3Prefix::parse,
-            take_until(" "),
-            Ircv3Params::parse,
-        ))(msg)?;
+struct Ircv3Parse;
+impl Ircv3Parse {
+    pub fn parse(msg: &str) -> IResult<&str, (IRCv3Tags, IRCv3Prefix, &str, IRCv3Params)> {
+        let (remain, (tags, prefix, command, params)) =
+            tuple((tags_parse, prefix_parse, take_until(" "), params_parse))(msg)?;
 
         Ok((
             remain,
-            Ircv3Parse {
-                tags,
-                prefix,
-                command,
-                params,
-            },
+            (tags, prefix, command, params), // },
         ))
     }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub struct Ircv3Prefix<'a> {
-    data: Option<(&'a str, Option<&'a str>)>,
-}
+/// tags, prefix, command, params
+pub fn ircv3_parse(msg: &str) -> (IRCv3Tags, IRCv3Prefix, &str, IRCv3Params) {
+    let (_, result) = Ircv3Parse::parse(msg).unwrap();
 
-impl<'a> Ircv3Prefix<'a> {
-    pub fn new(msg: &str) -> Ircv3Prefix {
-        let (_, data) = Ircv3Prefix::prefix_parse(msg).unwrap();
-
-        Ircv3Prefix { data }
-    }
-
-    pub fn parse(msg: &'a str) -> IResult<&str, Ircv3Prefix<'a>> {
-        let (msg, data) = Ircv3Prefix::prefix_parse(msg)?;
-
-        Ok((msg, Ircv3Prefix { data }))
-    }
-
-    pub fn to_string(self) -> Option<(String, Option<String>)> {
-        self.data.map(|value| {
-            let (server_nick, host) = value;
-            Some((server_nick.to_string(), host.map(str::to_string)))
-        })?
-    }
-
-    pub fn to_str(self) -> Option<(&'a str, Option<&'a str>)> {
-        self.data
-    }
-
-    fn prefix_parse(msg: &str) -> IResult<&str, Option<(&str, Option<&str>)>> {
-        opt(delimited(
-            tag(":"),
-            tuple((Ircv3Prefix::server_nick, Ircv3Prefix::opts_user)),
-            space1,
-        ))(msg)
-    }
-
-    fn server_nick(msg: &str) -> IResult<&str, &str> {
-        take_while(|c: char| !c.is_whitespace() && c != '!')(msg)
-    }
-
-    fn opts_user(msg: &str) -> IResult<&str, Option<&str>> {
-        opt(preceded(
-            tag("!"),
-            // take_while(|c: char| !c.is_whitespace() && c != '@'),
-            take_while(|c: char| !c.is_whitespace()),
-        ))(msg)
-    }
-
-    // fn opts_host(msg: &str) -> IResult<&str, Option<&str>> {
-    //     opt(preceded(tag("@"), take_until(" ")))(msg)
-    // }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub struct Ircv3Params<'a> {
-    data: &'a str,
-}
-
-impl<'a> Ircv3Params<'a> {
-    pub fn new(msg: &'a str) -> Ircv3Params<'a> {
-        Ircv3Params { data: msg }
-    }
-
-    pub fn parse(msg: &'a str) -> IResult<&str, Ircv3Params> {
-        let (remain, data) = delimited(space1, not_line_ending, alt((crlf, eof)))(msg)?;
-
-        Ok((remain, Ircv3Params { data }))
-    }
-
-    pub fn channel(&self) -> IResult<&str, &str> {
-        terminated(not_line_ending, alt((crlf, eof)))(self.data)
-    }
-
-    pub fn channel_n_message(&self) -> IResult<&str, ChannelnMsg> {
-        let (remain, (channel, _, message)) = tuple((
-            terminated(take_until(" "), space1),
-            tag(":"),
-            not_line_ending,
-        ))(self.data)?;
-
-        Ok((remain, ChannelnMsg::new(channel, message)))
-    }
-
-    pub fn middle_n_message(&self) -> IResult<&str, MiddlenMsg> {
-        let (remain, (middle, message)) =
-            tuple((terminated(take_until(":"), tag(":")), not_line_ending))(self.data)?;
-
-        Ok((remain, MiddlenMsg::new(middle.trim(), message)))
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct ChannelnMsg {
-    pub channel: String,
-    pub message: String,
-}
-
-impl ChannelnMsg {
-    pub fn new<T: Into<String>>(channel: T, message: T) -> ChannelnMsg {
-        ChannelnMsg {
-            channel: channel.into(),
-            message: message.into(),
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq)]
-pub struct MiddlenMsg {
-    pub middle: String,
-    pub message: String,
-}
-
-impl MiddlenMsg {
-    pub fn new<T: Into<String>>(middle: T, message: T) -> MiddlenMsg {
-        MiddlenMsg {
-            middle: middle.into(),
-            message: message.into(),
-        }
-    }
+    result
 }

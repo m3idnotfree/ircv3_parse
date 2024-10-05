a7e8c253cfd70431f5325b7c14663aecf7116f62
m3id <m3id.eprac@gmail.com>
2024-10-06 02:12:51 +0900

    improvement(prefix): struct update

--- src/prefix.rs
+++ src/prefix.rs
@@ -7,7 +7,11 @@ use nom::{
 };
 
 #[derive(Debug, Clone, PartialEq)]
-pub struct IRCv3Prefix((String, Option<String>));
+pub struct IRCv3Prefix {
+    pub servername_nick: String,
+    pub user: Option<String>,
+    pub host: Option<String>,
+}
 
 impl IRCv3Prefix {
     pub fn parse(msg: &str) -> IResult<&str, Option<IRCv3Prefix>> {
@@ -15,34 +19,37 @@ impl IRCv3Prefix {
 
         Ok((
             msg,
-            data.map(|x| IRCv3Prefix((x.0.to_string(), x.1.map(|x| x.to_string())))),
+            data.map(|x| IRCv3Prefix {
+                servername_nick: x.0.to_string(),
+                user: x.1.map(String::from),
+                host: x.2.map(String::from),
+            }),
         ))
     }
-
-    pub fn server_nick(&self) -> String {
-        self.0 .0.clone()
-    }
-
-    pub fn user(&self) -> Option<String> {
-        self.0 .1.clone()
-    }
-
-    // fn opts_host(msg: &str) -> IResult<&str, Option<&str>> {
-    //     opt(preceded(tag("@"), take_until(" ")))(msg)
-    // }
 }
 
-fn prefix_parse(msg: &str) -> IResult<&str, Option<(&str, Option<&str>)>> {
-    opt(delimited(tag(":"), tuple((server_nic, opts_user)), space1))(msg)
+fn prefix_parse(msg: &str) -> IResult<&str, Option<(&str, Option<&str>, Option<&str>)>> {
+    opt(delimited(
+        tag(":"),
+        tuple((server_nick, opts_user, opts_host)),
+        space1,
+    ))(msg)
 }
 
-fn server_nic(msg: &str) -> IResult<&str, &str> {
+fn server_nick(msg: &str) -> IResult<&str, &str> {
     take_while(|c: char| !c.is_whitespace() && c != '!')(msg)
 }
 
 fn opts_user(msg: &str) -> IResult<&str, Option<&str>> {
     opt(preceded(
         tag("!"),
+        take_while(|c: char| !c.is_whitespace() && c != '@'),
+        // take_while(|c: char| !c.is_whitespace()),
+    ))(msg)
+}
+fn opts_host(msg: &str) -> IResult<&str, Option<&str>> {
+    opt(preceded(
+        tag("@"),
         // take_while(|c: char| !c.is_whitespace() && c != '@'),
         take_while(|c: char| !c.is_whitespace()),
     ))(msg)

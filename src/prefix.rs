04cc76ec605f6fcf9a1406a36f4c8aefff3a7405
m3id <m3id.eprac@gmail.com>
2024-10-05 20:05:01 +0900

    refactor(prefix): parse move fn

--- src/prefix.rs
+++ src/prefix.rs
@@ -11,7 +11,7 @@ pub struct IRCv3Prefix<'a>(Option<(&'a str, Option<&'a str>)>);
 
 impl<'a> IRCv3Prefix<'a> {
     pub fn parse(msg: &str) -> IResult<&str, IRCv3Prefix> {
-        let (msg, data) = IRCv3Prefix::prefix_parse(msg)?;
+        let (msg, data) = prefix_parse(msg)?;
 
         Ok((msg, IRCv3Prefix(data)))
     }
@@ -31,26 +31,6 @@ impl<'a> IRCv3Prefix<'a> {
         self.0
     }
 
-    fn prefix_parse(msg: &str) -> IResult<&str, Option<(&str, Option<&str>)>> {
-        opt(delimited(
-            tag(":"),
-            tuple((IRCv3Prefix::server_nic, IRCv3Prefix::opts_user)),
-            space1,
-        ))(msg)
-    }
-
-    fn server_nic(msg: &str) -> IResult<&str, &str> {
-        take_while(|c: char| !c.is_whitespace() && c != '!')(msg)
-    }
-
-    fn opts_user(msg: &str) -> IResult<&str, Option<&str>> {
-        opt(preceded(
-            tag("!"),
-            // take_while(|c: char| !c.is_whitespace() && c != '@'),
-            take_while(|c: char| !c.is_whitespace()),
-        ))(msg)
-    }
-
     // fn opts_host(msg: &str) -> IResult<&str, Option<&str>> {
     //     opt(preceded(tag("@"), take_until(" ")))(msg)
     // }
@@ -60,4 +40,20 @@ impl<'a> AsRef<Option<(&'a str, Option<&'a str>)>> for IRCv3Prefix<'a> {
     fn as_ref(&self) -> &Option<(&'a str, Option<&'a str>)> {
         &self.0
     }
+}
+
+fn prefix_parse(msg: &str) -> IResult<&str, Option<(&str, Option<&str>)>> {
+    opt(delimited(tag(":"), tuple((server_nic, opts_user)), space1))(msg)
+}
+
+fn server_nic(msg: &str) -> IResult<&str, &str> {
+    take_while(|c: char| !c.is_whitespace() && c != '!')(msg)
+}
+
+fn opts_user(msg: &str) -> IResult<&str, Option<&str>> {
+    opt(preceded(
+        tag("!"),
+        // take_while(|c: char| !c.is_whitespace() && c != '@'),
+        take_while(|c: char| !c.is_whitespace()),
+    ))(msg)
 }

bf824419edfbc74ce8affcb56d39349be898edd7
m3id <m3id.eprac@gmail.com>
2024-10-07 12:10:22 +0900

    refactor(params): all new

--- src/params.rs
+++ src/params.rs
@@ -1,74 +1,85 @@
 use nom::{
     branch::alt,
-    bytes::complete::{tag, take_until},
+    bytes::complete::{tag, take_until, take_while},
     character::complete::{crlf, not_line_ending, space1},
-    combinator::{eof, not, opt},
-    sequence::{preceded, terminated, tuple},
+    combinator::opt,
+    sequence::preceded,
     IResult,
 };
 
-#[derive(Debug, Clone, PartialEq)]
-pub struct IRCv3Params<'a>(Option<(&'a str, Option<&'a str>)>);
+#[derive(Debug, Clone, PartialEq, Default)]
+pub struct IRCv3Params {
+    pub channel: Option<String>,
+    pub middle: Vec<String>,
+    pub message: Option<String>,
+}
 
-impl<'a> IRCv3Params<'a> {
-    pub fn parse(msg: &str) -> IResult<&str, IRCv3Params> {
-        let (remain, data) = opt(preceded(
-            space1,
-            terminated(
-                alt((
-                    IRCv3Params::channel_and_message,
-                    IRCv3Params::middle_and_message,
-                    IRCv3Params::only_channel,
-                )),
-                alt((crlf, eof)),
-            ),
-        ))(msg)?;
+pub fn params_parse(msg: &str) -> IResult<&str, IRCv3Params> {
+    let a = IRCv3Params::default();
+    params_inner(msg, a)
+}
 
-        Ok((remain, IRCv3Params(data)))
-    }
+pub fn params_inner(msg: &str, mut ircv3_params: IRCv3Params) -> IResult<&str, IRCv3Params> {
+    if msg.is_empty() {
+        Ok((msg, ircv3_params))
+    } else {
+        let (remain, empty_crlf) = opt(msg_crlf)(msg)?;
+        if let Some(empty_crlf) = empty_crlf {
+            // Ok((empty_crlf, ircv3_params))
+            Ok((remain, ircv3_params))
+        } else {
+            let (remain, (message, channel, middle)) =
+                preceded(space1, alt((trailing, channel, middle)))(remain)?;
+            if message.is_some() {
+                ircv3_params.message = message.map(String::from);
+            }
 
-    pub fn get(&self) -> Option<(&'a str, Option<&'a str>)> {
-        self.0
-    }
+            if channel.is_some() && ircv3_params.channel.is_none() {
+                let is_eq = ircv3_params.middle.pop();
 
-    pub fn channel(&self) -> Option<&str> {
-        self.0.map(|value| value.0)
-    }
+                let c = if is_eq.is_some() && is_eq.unwrap() == "=" {
+                    let rename = ircv3_params.middle.pop().unwrap();
+                    Some(format!("{rename} = {}", channel.unwrap()))
+                } else {
+                    channel.map(String::from)
+                };
+                ircv3_params.channel = c;
+            }
 
-    pub fn message(&self) -> Option<&str> {
-        match self.0 {
-            None => None,
-            Some(value) => value.1,
+            if let Some(middle) = middle {
+                ircv3_params.middle.push(middle.to_string());
+            }
+            params_inner(remain, ircv3_params)
         }
-    }
-
-    fn channel_and_message(msg: &str) -> IResult<&str, (&str, Option<&str>)> {
-        let (msg, (channel, message)) = tuple((
-            terminated(take_until(" "), space1),
-            preceded(tag(":"), not_line_ending),
-        ))(msg)?;
-
-        Ok((msg, (channel, Some(message))))
-    }
-
-    fn middle_and_message(msg: &str) -> IResult<&str, (&str, Option<&str>)> {
-        let (remain, (middle, message)) =
-            tuple((terminated(take_until(":"), tag(":")), not_line_ending))(msg)?;
-
-        Ok((remain, (middle.trim(), Some(message))))
-    }
-
-    fn only_channel(msg: &str) -> IResult<&str, (&str, Option<&str>)> {
-        not(eof)(msg)?;
-        not(crlf)(msg)?;
-
-        let (msg, channel) = not_line_ending(msg)?;
-        Ok((msg, (channel, None)))
     }
 }
 
-impl<'a> AsRef<Option<(&'a str, Option<&'a str>)>> for IRCv3Params<'a> {
-    fn as_ref(&self) -> &Option<(&'a str, Option<&'a str>)> {
-        &self.0
-    }
+type ParamsReturn<'a> = IResult<&'a str, (Option<&'a str>, Option<&'a str>, Option<&'a str>)>;
+/// (remain, (empty_crlf, message, channel ,middle))
+fn msg_empty(msg: &str) -> IResult<&str, &str> {
+    tag("")(msg)
+}
+
+/// (remain, (empty_crlf, message, channel ,middle))
+fn msg_crlf(msg: &str) -> IResult<&str, &str> {
+    crlf(msg)
+}
+
+/// (remain, (message, channel ,middle))
+fn trailing(msg: &str) -> ParamsReturn {
+    let (remain, message) = preceded(tag(":"), not_line_ending)(msg)?;
+
+    Ok((remain, (Some(message), None, None)))
+}
+/// (remain, (message, channel ,middle))
+fn channel(msg: &str) -> ParamsReturn {
+    let (remain, channel) = preceded(tag("#"), alt((take_until(" "), not_line_ending)))(msg)?;
+
+    Ok((remain, (None, Some(channel), None)))
+}
+/// (remain, (message, channel, middle))
+fn middle(msg: &str) -> ParamsReturn {
+    let (remain, middle) = take_while(|c: char| c != ':' && !c.is_whitespace())(msg)?;
+
+    Ok((remain, (None, None, Some(middle))))
 }

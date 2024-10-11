d08db8932ed4198d08b682c61572262d9256de6b
m3id <m3id.eprac@gmail.com>
2024-10-11 21:07:07 +0900

    chore(lib): update doc

--- src/lib.rs
+++ src/lib.rs
@@ -48,6 +48,68 @@
 //! assert_eq!(None, channel.alt);
 //!
 //!```
+//!
+//! # Custom params middle parse
+//!```no_run
+//! use ircv3_parse::{IRCv3Builder, ParamsParse};
+//! use nom::{
+//!     branch::alt,
+//!     bytes::complete::{tag, take_until},
+//!     character::complete::{not_line_ending, space1},
+//!     sequence::tuple,
+//!     IResult,
+//!};
+//!
+//! let msg = ":foo!foo@foo.tmi.abcdef.gh PRIVMSG guest w :bleedPurple";
+//! let result = IRCv3Builder::new(WhoAmI::default()).parse(msg);
+//!
+//! assert_eq!("guest".to_string(), result.params.stats);
+//! assert_eq!("w".to_string(), result.params.user);
+//! #[derive(Default)]
+//! struct WhoAmI {
+//!     pub stats: String,
+//!     pub user: String,
+//! }
+//!
+//! impl ParamsParse for WhoAmI {
+//!     fn parse(&self, _: &str, middle: ircv3_parse::IRCv3ParamsBase) -> Self
+//!     where
+//!         Self: Sized,
+//!     {
+//!         let join_middle = middle.middle.join(" ");
+//!         let (_, (who, user)) = whoami(join_middle.as_str()).unwrap();
+//!         WhoAmI {
+//!             stats: who.to_string(),
+//!             user: user.to_string(),
+//!         }
+//!     }
+//! }
+//!
+//! pub fn whoami(msg: &str) -> IResult<&str, (&str, &str)> {
+//!     let (remain, who) = alt((owner_user, guest_user))(msg)?;
+//!     Ok((remain, who))
+//! }
+//!
+//! fn owner_user(msg: &str) -> IResult<&str, (&str, &str)> {
+//!     let (remain, (gust, _, user)) = tuple((
+//!         tag("owner"),
+//!         space1,
+//!         alt((take_until(" "), not_line_ending)),
+//!     ))(msg)?;
+//!
+//!     Ok((remain, (gust, user)))
+//! }
+//!
+//! fn guest_user(msg: &str) -> IResult<&str, (&str, &str)> {
+//!     let (remain, (gust, _, user)) = tuple((
+//!         tag("guest"),
+//!         space1,
+//!         alt((take_until(" "), not_line_ending)),
+//!     ))(msg)?;
+//!
+//!     Ok((remain, (gust, user)))
+//! }
+//! ```
 
 mod source;
 use std::collections::VecDeque;

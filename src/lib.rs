398c463abfd3ba348f386e7b198e316b8279b706
m3id <m3id.eprac@gmail.com>
2024-10-11 20:03:49 +0900

    feat(lib): add IRCv3 struct
    - refactor: IRCv3Mesage move file
    - test update

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,10 @@
 //! # Example
 //! ```no_run
 //! use std::collections::HashMap;
+//! use ircv3_parse::{Channel, IRCv3};
 //!
 //! let msg = "@badge-info=;badges=broadcaster/1;client-nonce=997dcf443c31e258c1d32a8da47b6936;color=#0000FF;display-name=abc;emotes=;first-msg=0;flags=0-6:S.7;id=eb24e920-8065-492a-8aea-266a00fc5126;mod=0;room-id=713936733;subscriber=0;tmi-sent-ts=1642786203573;turbo=0;user-id=713936733;user-type= :abc!abc@abc.tmi.twitch.tv PRIVMSG #xyz :HeyGuys\r\n";
-//! let ircv3_message = ircv3_parse::parse(msg);
+//! let ircv3_message = IRCv3::parse(msg);
 //! let expeced_tags= HashMap::from([
 //!     ("badge-info", ""),
 //!     ("subscriber", "0"),
@@ -26,79 +27,50 @@
 //!
 //!
 //!
-//! assert!(ircv3_message.prefix.is_some());
-//! let prefix = ircv3_message.prefix.unwrap();
-//! assert_eq!(prefix.servername_nick, "abc");
-//! assert_eq!(prefix.user, Some("abc".to_string()));
-//! assert_eq!(prefix.host, Some("abc.tmi.twitch.tv".to_string()));
+//! assert!(ircv3_message.tags.is_some());
+//! let tags = ircv3_message.tags.clone().unwrap();
+//! assert_eq!(Some("id".to_string()), tags.get("id"));
+//! assert_eq!(None, tags.get("n"));
 //!
-//! let params = ircv3_message.params;
-//! assert_eq!(ircv3_message.command, "PRIVMSG".to_string());
-//! assert_eq!(params.channel, Some("xyz".to_string()));
-//! assert_eq!(params.message, Some("HeyGuys".to_string()));
+//! assert!(ircv3_message.source.is_some());
+//! let source = ircv3_message.source.clone().unwrap();
+//! assert_eq!("abc", source.servername_nick);
+//! assert_eq!(Some("abc".to_string()), source.user);
+//! assert_eq!(Some("abc.tmi.twitch.tv".to_string()), source.host);
+//!
+//! assert_eq!("PRIVMSG".to_string(),ircv3_message.command);
+//!
+//! let mut params = ircv3_message.params.clone();
+//! assert_eq!(Some("HeyGuys".to_string()), params.message);
+//!
+//! let channel = params.channel.unwrap();
+//! assert_eq!("#xyz".to_string(), channel.name);
+//! assert_eq!(None, channel.alt);
 //!
 //!```
 
+mod source;
 use std::collections::VecDeque;
 
-use ircv3_tags::IRCv3Tags;
-use nom::sequence::tuple;
-
-mod source;
 pub use source::*;
 mod command;
 pub use command::*;
 mod params;
 pub use params::*;
+mod builder;
+pub use builder::*;
+mod message;
+pub use message::*;
 
 #[derive(Debug)]
-pub struct IRCv3Message {
-    pub tags: Option<IRCv3Tags>,
-    pub source: Option<IRCv3Source>,
-    pub command: String,
-    pub params: IRCv3Params,
-}
+pub struct IRCv3;
 
-pub fn parse(msg: &str) -> IRCv3Message {
-    let (_, (tags, source, command, params)) = tuple((
-        ircv3_tags::parse_nom,
-        source_parse,
-        command_parse,
-        params_parse,
-    ))(msg)
-    .unwrap();
-
-    IRCv3Message {
-        tags,
-        source,
-        command: command.to_string(),
-        params,
+impl IRCv3 {
+    pub fn parse(msg: &str) -> IRCv3Message<IRCv3Params> {
+        IRCv3Builder::default().parse(msg)
     }
-}
 
-pub fn parse_vecdeque(msg: &str) -> VecDeque<IRCv3Message> {
-    parse_inner(msg, VecDeque::new())
-}
-
-fn parse_inner(msg: &str, mut result: VecDeque<IRCv3Message>) -> VecDeque<IRCv3Message> {
-    if msg.is_empty() {
-        result
-    } else {
-        let (msg, (tags, source, command, params)) = tuple((
-            ircv3_tags::parse_nom,
-            source_parse,
-            command_parse,
-            params_parse,
-        ))(msg)
-        .unwrap();
-
-        result.push_back(IRCv3Message {
-            tags,
-            source,
-            command: command.to_string(),
-            params,
-        });
-
-        parse_inner(msg, result)
+    pub fn parse_vecdeque(msg: &str) -> VecDeque<IRCv3Message<IRCv3Params>> {
+        IRCv3Builder::default().parse_vecdeque(msg)
     }
 }

df791c9952e7f8cbe0d0d31d9a669e0b309c5c4a
m3id <m3id.eprac@gmail.com>
2024-10-11 19:59:39 +0900

    feat(message): base and generic struct

---
+++ src/message.rs
@@ -0,0 +1,28 @@
+use ircv3_tags::IRCv3Tags;
+
+use crate::{IRCv3ParamsBase, IRCv3Source, ParamsParse};
+
+#[derive(Debug)]
+pub struct IRCv3Message<T> {
+    pub tags: Option<IRCv3Tags>,
+    pub source: Option<IRCv3Source>,
+    pub command: String,
+    pub params: T,
+}
+
+#[derive(Debug)]
+pub struct IRCv3MessageBase {
+    pub tags: Option<IRCv3Tags>,
+    pub source: Option<IRCv3Source>,
+    pub command: String,
+    pub params: IRCv3ParamsBase,
+}
+
+impl IRCv3MessageBase {
+    pub fn params_middle_parse<F>(&self, f: &F) -> F
+    where
+        F: ParamsParse,
+    {
+        f.parse(self.command.as_str(), self.params.clone())
+    }
+}

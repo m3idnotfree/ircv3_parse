55b9bed2612ab65aa3d98c6193a3863f02b3b340
m3id <m3id.eprac@gmail.com>
2026-02-10 14:32:40 +0900

    test: reorganize test into de/ser structure with clear naming

--- ircv3_parse_derive/tests/compile_fail.rs
+++ ircv3_parse_derive/tests/compile_fail.rs
@@ -1,6 +1,13 @@
 #[test]
-fn ui() {
+fn ui_de() {
     let t = trybuild::TestCases::new();
-    t.compile_fail("tests/ui/fail/*.rs");
-    t.pass("tests/ui/pass/*.rs");
+    t.compile_fail("tests/ui/de/fail/*.rs");
+    t.pass("tests/ui/de/pass/*.rs");
+}
+
+#[test]
+fn ui_ser() {
+    let t = trybuild::TestCases::new();
+    t.compile_fail("tests/ui/ser/fail/*.rs");
+    t.pass("tests/ui/ser/pass/*.rs");
 }
---
+++ ircv3_parse_derive/tests/de.rs
@@ -0,0 +1,20 @@
+#[path = "de/components/command.rs"]
+mod de_components_command;
+
+#[path = "de/components/tag.rs"]
+mod de_components_tag;
+
+#[path = "de/components/source.rs"]
+mod de_components_source;
+
+#[path = "de/components/param.rs"]
+mod de_components_param;
+
+#[path = "de/components/trailing.rs"]
+mod de_components_trailing;
+
+#[path = "de/structs/named.rs"]
+mod de_structs_named;
+
+#[path = "de/structs/unnamed.rs"]
+mod de_structs_unnamed;
